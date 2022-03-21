a8a06b2bcced0da9f8909a972c81d74b469b8238
ephong93 <ephong93@gmail.com>
2022-03-21 23:36:10 +0900

    Add onChange for authr and content

--- client/src/components/CommentForm.js
+++ client/src/components/CommentForm.js
@@ -1,10 +1,31 @@
+import { useState } from 'react'
+
 const CommentForm = () => {
+  const [author, setAuthor] = useState('')
+  const [content, setContent] = useState('')
+
+  const onChangeAuthor = (e) => {
+    setAuthor(e.target.value)
+  }
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value)
+  }
+
   return (
     <div>
       <label htmlFor='author'>Author</label>
-      <input id='author' />
+      <input
+        id='author'
+        onChange={onChangeAuthor}
+        value={author}
+      />
       <label htmlFor='content'>Content</label>
-      <input id='content' />
+      <input
+        id='content'
+        onChange={onChangeContent}
+        value={content}
+      />
     </div>
   )
 }
--- client/src/components/CommentForm.test.js
+++ client/src/components/CommentForm.test.js
@@ -1,10 +1,21 @@
 import CommentForm from './CommentForm'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 describe('CommentForm', () => {
   it('renders text inputs for author and content', () => {
     render(<CommentForm />)
     screen.getByRole('textbox', {name: /author/i})
     screen.getByRole('textbox', {name: /content/i})
+  })
+  it('changes values of author and content when user types', () => {
+    render(<CommentForm />)
+    const authorInput = screen.getByRole('textbox', {name: /author/i})
+    userEvent.type(authorInput, 'Roy')
+    expect(authorInput.value).toBe('Roy')
+
+    const contentInput = screen.getByRole('textbox', {name: /content/i})
+    userEvent.type(contentInput, 'Nice to meet you')
+    expect(contentInput.value).toBe('Nice to meet you')
   })
 })

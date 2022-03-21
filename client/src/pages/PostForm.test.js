a2ee3eabcee0f0e47b2e2556b18aa93ade1e393f
ephong93 <ephong93@gmail.com>
2022-03-21 20:40:15 +0900

    Add onSubmit test

--- client/src/pages/PostForm.test.js
+++ client/src/pages/PostForm.test.js
@@ -1,7 +1,9 @@
 import PostForm from './PostForm'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 describe('PostForm', () => {
+  const onSubmit = jest.fn()
   it('renders inputs and labels', () => {
     render(<PostForm />)
     screen.getByRole('textbox', {name: /title/i})
@@ -10,5 +12,11 @@ describe('PostForm', () => {
   it('renders submit button', () => {
     render(<PostForm />)
     screen.getByRole('button', {name: /submit/i})
+  })
+  it('calls onSubmit if submit button is clicked', () => {
+    render(<PostForm onSubmit={onSubmit} />)
+    const button = screen.getByRole('button', {name: /submit/i})
+    userEvent.click(button)
+    expect(onSubmit).toBeCalled()
   })
 })

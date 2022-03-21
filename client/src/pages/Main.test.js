53974dcaa99a1407bd3eeb687d383a9a946cac5d
ephong93 <ephong93@gmail.com>
2022-03-21 22:09:42 +0900

    Add mock useHistory

--- client/src/pages/Main.test.js
+++ client/src/pages/Main.test.js
@@ -1,9 +1,24 @@
 import Main from './Main'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+const mockHistory = {
+  push: jest.fn()
+}
+
+jest.mock('react-router-dom', () => ({
+  useHistory: () => mockHistory
+}))
 
 describe('Main', () => {
   it('renders post button', () => {
     render(<Main />)
     screen.getByRole('button', {name: /post/i})
+  })
+  it('calls history.push when clicking post button', () => {
+    render(<Main />)
+    const postButton = screen.getByRole('button', {name: /post/i})
+    userEvent.click(postButton)
+    expect(mockHistory.push).toBeCalledWith('/post-form')
   })
 })

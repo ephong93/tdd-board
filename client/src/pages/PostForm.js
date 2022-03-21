930c701ef25b26b994488be9e4361887ecc35cc1
ephong93 <ephong93@gmail.com>
2022-03-21 20:41:49 +0900

    Implement onSubmit

--- client/src/pages/PostForm.js
+++ client/src/pages/PostForm.js
@@ -1,9 +1,20 @@
-const PostForm = () => {
+import { useState } from 'react'
+
+const PostForm = ({
+  onSubmit
+}) => {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
   return (
     <div>
       <label htmlFor='title'>Title</label><input id='title' type='text' />
       <label htmlFor='content'>Content</label><textarea id='content' />
-      <button>Submit</button>
+      <button
+        onClick={() => onSubmit({ title, content })}
+      >
+        Submit
+      </button>
     </div>
   )
 }
